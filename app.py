92b8dc1bab929a43006b5420b22168abd582dc66
sri hari vasan <sriharivasanop@gmail.com>
2025-04-30 21:44:26 +0530

    ui_added
    added the user interface for the model

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template
+import joblib
+
+app = Flask(__name__)
+
+model = joblib.load('spam_classifier_model.pkl')
+vectorizer = joblib.load('tfidf_vectorizer.pkl')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    message = request.form['message']
+    transformed = vectorizer.transform([message])
+    prediction = model.predict(transformed)
+    result = "HAM" if prediction[0] == 1 else "SPAM"
+    return render_template('index.html', prediction=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
